b49f6d0f3e598ebfb57585cbe22be8e34f4f25a9
MrGoropeza <goropeza8@gmail.com>
2022-08-11 11:53:04 -0300

    importado al enviroment la clave y url del proyecto de supabase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltbHdjYnZrbnZjeG5ueHVka2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjkzMjMsImV4cCI6MTk3NTgwNTMyM30.lpgWC6Q9c27sBkTMCMLSVdCbGrWwQew5NNk-0E9n-jU",
+  supabaseUrl: "https://ymlwcbvknvcxnnxudkar.supabase.co"
 };
 
 /*
