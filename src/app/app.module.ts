a1c7d92e1558488f0ad70aff31b232c39608207b
MrGoropeza <goropeza8@gmail.com>
2022-08-19 18:02:59 -0300

    delete: borrado el componente abm-test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AbmTestComponent } from './pages/abm-test/abm-test.component';
-import { NavbarComponent } from './pages/components/navbar/navbar.component';
+import { NavbarComponent } from './pages/global-components/navbar/navbar.component';
 import { PrimengImportsModule } from './primeng-imports/primeng-imports.module';
-import { SidebarComponent } from './pages/components/sidebar/sidebar.component';
+import { SidebarComponent } from './pages/global-components/sidebar/sidebar.component';
 import { ArticuloPageComponent } from './pages/articulo-page/articulo-page.component';
 import { DepositosPageComponent } from './pages/depositos-page/depositos-page.component';
 import { DepositoSeleccionadoPageComponent } from './pages/deposito-seleccionado-page/deposito-seleccionado-page.component';
@@ -16,7 +15,6 @@ import { DepositoSeleccionadoPageComponent } from './pages/deposito-seleccionado
 @NgModule({
   declarations: [
     AppComponent,
-    AbmTestComponent,
     NavbarComponent,
     SidebarComponent,
     ArticuloPageComponent,
