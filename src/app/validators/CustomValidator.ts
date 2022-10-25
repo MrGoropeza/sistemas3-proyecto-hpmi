fc9789da9b6b7edc9a8bac9880bf85794aed0eb9
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-10-25 11:35:41 -0300

    fix: expresiones regulares

--- src/app/validators/CustomValidator.ts
+++ src/app/validators/CustomValidator.ts
@@ -3,6 +3,11 @@ import { ArticuloComprobante } from "../models/ArticuloComprobante";
 
 export const CustomValidator = /^(20|23|27|30|33)([0-9]{9}|-[0-9]{8}-[0-9]{1})$/g;
 
+export const correoValidator = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+export const dniValidator = /^\d{8}(?:[-\s]\d{4})?$/;
+
+export const telefonoValidator = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/;
 
 export const NroFacturaRegExp = /^\d{4}(-)\d+/;
 
