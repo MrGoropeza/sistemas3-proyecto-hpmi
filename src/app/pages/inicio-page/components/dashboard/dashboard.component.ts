197e18cbd5522d1907ad4ac218458fe7699047b5
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-10-27 12:25:04 -0300

    fix: add paciente

--- src/app/pages/inicio-page/components/dashboard/dashboard.component.ts
+++ src/app/pages/inicio-page/components/dashboard/dashboard.component.ts
@@ -7,11 +7,29 @@ import { ChartService } from 'src/app/services/charts/chart.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  cantPacientes : number = 0;
   constructor(private chartService : ChartService) { }
 
   ngOnInit(): void {
+    const now = new Date();
+    const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+    const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+    const primerDia = firstDay.toISOString();
+    const ultimoDia = lastDay.toISOString();
+    this.getCantPacientes(primerDia,ultimoDia);
   }
-  
+  async getCantPacientes(primerDia : string,ultimoDia : string){
+    let request = await this.chartService.getCantPacientes(primerDia,ultimoDia);
+    if(request.data){
+      console.log(request.data);
+      this.cantPacientes = request.data.length;
+      
+    }else{
+      console.log(request.error);
+    }
+  }
+  getCantAtenciones(){
+
+  }
 
 }
--- src/app/services/charts/chart.service.ts
+++ src/app/services/charts/chart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { SupabaseClient } from "@supabase/supabase-js";
+import { Paciente } from "src/app/models/Paciente";
 import { SupabaseService } from "../supabase.service";
 
 @Injectable({
@@ -11,5 +12,14 @@ export class ChartService {
   constructor(private supabaseService: SupabaseService) {
     this.supabase = this.supabaseService.getSupabaseClient();
   }
-  
+  async getCantPacientes(primerDia : string,ultimoDia : String){
+    return await this.supabase
+    .from("Paciente")
+    .select("*", { count: 'exact'})
+    .eq("estado", true)
+    .gt('fechaIngreso',primerDia)
+    .lte('fechaIngreso',ultimoDia);
+
+  }
+
 }
